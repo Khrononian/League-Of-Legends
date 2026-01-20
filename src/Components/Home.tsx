ecde825a32aa5502621e391a29810883d3087d26
Alvin <Alv.Barnes@gmail.com>
2026-01-20 18:38:13 -0500

    Removed unused imports

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Nav from './Nav'
 import '../styles/Home.css'
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import { HugeiconsIcon } from '@hugeicons/react'
 import { Menu02Icon } from '@hugeicons/core-free-icons'
