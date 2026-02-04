6a381513558142637a098158c4ac13ad9dc735f6
Alvin <Alv.Barnes@gmail.com>
2026-02-03 23:03:35 -0500

    Added main request functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,59 +9,104 @@
 // import { logger } from "firebase-functions/logger";
 // import {setGlobalOptions} from "firebase-functions";
 import { onRequest } from "firebase-functions/https";
-import { ParsedQs } from 'qs'
+// import { ParsedQs } from 'qs'
 import { defineSecret } from "firebase-functions/params";
 
-
-
+type MatchHistory = {
+    info: {
+        endOfGameResult: string
+        gameStartTimestamp: number,
+        gameEndTimestamp: number,
+        gameMode: string,
+        championName: string,
+        gameId: number
+        participants: [
+            {
+                puuid: string, 
+                teamId: number,
+                win: boolean,
+                riotIdGameName: string,
+                riotIdTagline: string,
+                summoner1Id: number,
+                summoner2Id: number,
+                championName: string,
+                champLevel: number,
+                kills: number,
+                deaths: number,
+                assists: number,
+                detectorWardsPlaced: number,
+                wardsPlaced: number,
+                wardsKilled:number,
+                neutralMinionsKilled: number,
+                totalMinionsKilled: number,
+                goldEarned: number,
+                timePlayed: number,
+                item: string | number[],
+                item0: number,
+                item1: number,
+                item2: number,
+                item3: number,
+                item4: number,
+                item5: number,
+                item6: number,
+                challenges: {
+                    kda: number,
+                    killParticipation: number
+                },
+                perks: {
+                    styles: [
+                        {selections: [
+                            {perk: number}
+                        ],
+                        style: number
+                    },
+                    {selections: [
+                            {perk: number}
+                        ],
+                        style: number
+                    },
+                    ]
+                }
+            }
+        ]
+        teams: [
+            teamId: number,
+            win: boolean,
+        ]
+    }
+}
 const RIOT_APIKEY = defineSecret('RIOT_APIKEY')
 
+// const normalizeQueryParam = (value: string | string[] | ParsedQs | ParsedQs[] |
+//     undefined
+// ): string | undefined => {
+//     if (typeof value === 'string') return value.trim() || undefined
+//     if (Array.isArray(value)) {
+//         const first = value[0]
 
+//         if (typeof first === 'string') return first.trim() || undefined
+        
+//         return undefined
+//     }
+    
+//     return undefined
+// }
+
+// const normalizeIdQuery = () => {
+
+// }
 export const getAccount = onRequest(
     { secrets: [RIOT_APIKEY], cors: true },
     async (req, res) => {
         try {
+            // const summonerName = normalizeQueryParam(req.query)
+            const summonerName = typeof req.query.summoner === 'string' ? req.query.summoner.trim() : ''
             const apiKey = RIOT_APIKEY.value()
-            // const summonerName = req.query.summoner
-            // logger.info("Incoming request:", { url: req.originalUrl, query: req.query });
-            // logger.info("Summoner:", req.query.summoner);
-            
-            
-            const normalizeQueryParam = (value: string | string[] | ParsedQs | ParsedQs[] |
-                undefined
-            ): string | undefined => {
-                if (typeof value === 'string') return value.trim() || undefined
-                
-                if (Array.isArray(value)) {
-                    const first = value[0]
-
-                    if (typeof first === 'string') return first.trim() || undefined
-                    
-                    return undefined
-                }
-                
-                return undefined
-            }
-            // const summonerName = normalizeQueryParam(
-            //     Array.isArray(req.query.summoner) ? req.query.summoner[0]
-            //     : typeof req.query.summoner === 'string' ? req.query.summoner
-            //     : undefined
-            // )
-            const summonerName = normalizeQueryParam(req.query)
             // // const summonerName = 'selbull'
             if (!summonerName?.trim() ) {
-                // res.status(400).json({error: 'MISSING NAME', key: apiKey})
-                console.log('FIREBASE')
-                console.log('QUERY', req, req.query, )
                 res.status(400).json({error: 'MISSING NAMEE', key: apiKey, name: summonerName})
                 return
             }
-
-            // if (!apiKey) {
-            //     res.status(500).json({error: 'MISSING API', key: apiKey})
-            //     return
-            // }
-            // const summonerName = req.query.summoner
 
             const riotRes = await fetch(
                 `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(
@@ -70,22 +115,128 @@ export const getAccount = onRequest(
             )
             const response = await riotRes.json() 
             
-            // res.json(response)
             res.status(200).json(response)
             return
         } catch (error) {
             console.error(error)
             res.status(500).json({ error: 'Internal error'})
         }
-    
-        
-
-        // res.json({
-        //     secretExists: !!apiKey,
-        //     secondary: apiKey,
-        //     final: apiKey.toLowerCase()
-        // })
 })
+
+export const getAccountId = onRequest(
+    { secrets: [RIOT_APIKEY], cors: true },
+    async (req, res) => {
+        try {
+            // const summonerId = normalizeQueryParam(req.query)
+            const summonerId = typeof req.query.summoner === 'string' ? req.query.summoner.trim() : ''
+            const apiKey = RIOT_APIKEY.value()
+
+            if (!summonerId ) {
+                res.status(400).json({error: 'MISSING ID', key: apiKey, name: summonerId})
+                return
+            }
+
+            const riotRes = await fetch(
+                `https://americas.api.riotgames.com/riot/account/v1/accounts/by-puuid/${encodeURIComponent(
+                    summonerId
+                )}?api_key=${apiKey}`
+            )
+
+            const response = await riotRes.json()
+
+            res.status(200).json(response)
+            return
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({ error: 'Internal error'})
+        }
+    }
+)
+
+export const getAccountProfile = onRequest(
+    { secrets: [RIOT_APIKEY], cors: true },
+    async (req, res) => {
+        try {
+            const puuid = typeof req.query.puuid === 'string' ? req.query.puuid.trim() : ''
+            const apiKey = RIOT_APIKEY.value()
+
+            if (!puuid ) {
+                res.status(400).json({error: 'MISSING ID', key: apiKey, name: puuid})
+                return
+            }
+
+            const accountProfileResponse = await fetch(
+                `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${encodeURIComponent(
+                    puuid
+                )}?api_key=${apiKey}`
+            )
+            const profileResponse = await accountProfileResponse.json()
+            const accountRankedResponse = await fetch(
+                `https://na1.api.riotgames.com/lol/league/v4/entries/by-puuid/${encodeURIComponent(
+                    puuid
+                )}?api_key=${apiKey}`
+            )
+            const rankedResponse = await accountRankedResponse.json()
+            
+            const accountMatchHistory = await fetch(
+                `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${encodeURIComponent(
+                    puuid
+                )}/ids?start=0&count=20&api_key=${apiKey}`
+            )
+            const matchHistoryResponse = await accountMatchHistory.json()
+
+            // const allApis = Promise.all([accountProfileResponse, accountRankedResponse, accountMatchHistory].map(async (url) => {
+            //     await url.json()
+            // }))
+
+            const fetchMatchHistory = async (): Promise<MatchHistory[][]> => {
+                const matchHistoryResults = []
+
+                // return Promise.all(
+                //     matchHistoryResponse.slice(0, 5).map(async (id: string) => {
+                //         const matchResponse = await fetch(
+                //             `https://americas.api.riotgames.com/lol/match/v5/matches/${id}?api_key=${apiKey}`
+                //         )
+
+                //         return matchResponse.json()
+                //     })
+                // )
+                for (let i = 0; i < matchHistoryResponse.length; i += 5) {
+                    const sliced = matchHistoryResponse.slice(i, i + 5)
+
+                    const sliceSize = await Promise.all(
+                        sliced.map((id: string) => fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/${id}?api_key=${apiKey}`)
+                    .then(response => response.json()))
+                    )
+
+                    sliceSize.map(item => delete item.metadata)
+                    matchHistoryResults.push(sliceSize)
+
+                    // matchHistoryResults.map((item, index) => {
+                    //     console.log('FULLSTRAIN', item, index)
+
+                    //     item.map(element => console.log('FULLINNER', element))
+                    // })
+                    
+                    await new Promise(resolve => setTimeout(resolve, 800))
+                }
+
+                return matchHistoryResults
+                // return Promise.all(matchHistoryResults)
+                
+            }
+            const fetchedResults = await fetchMatchHistory()
+            // res.status(200).json(profileResponse)
+            // res.status(200).json(rankedResponse) 
+            res.status(200).json({ profileResponse, rankedResponse, matchHistoryResponse, fetchedResults }) 
+            // res.status(200).json({ allApis }) 
+            return
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({ error: 'Internal error'})
+        }
+    }
+)
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
