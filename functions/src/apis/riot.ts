973b468a9be6b5b378bd982a7850792007452cb1
Alvin <Alv.Barnes@gmail.com>
2026-02-03 23:03:11 -0500

    Added bridge functions

--- functions/src/apis/riot.ts
+++ functions/src/apis/riot.ts
@@ -1,19 +1,42 @@
-export const getAccount = async (summoner: string) => {
-// export const getAccount = async () => {
+const mainUrl = 'https://us-central1-lol-apis-c196.cloudfunctions.net/'
 
+export const getAccount = async (summoner: string) => {
     const res = await fetch(
-        `https://us-central1-lol-apis-c196.cloudfunctions.net/getAccount?summoner=${encodeURIComponent(
+        `${mainUrl}getAccount?summoner=${encodeURIComponent(
             summoner
-        )}`, {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json",
-            }
-        }
+        )}`
         
         // `https://us-central1-lol-apis-c196.cloudfunctions.net/getAccount?summoner=selbull`
     )
+    console.log('PUUID NAME', summoner, res)
     if (!res.ok) throw new Error (`Failed to fetch account ${res.status}`)
 
     return res.json()
 }
+
+export const getAccountId = async (puuid: string) => {
+    const res = await fetch(
+        `${mainUrl}getAccountId?puuid=${encodeURIComponent(
+            puuid
+        )}`
+    )
+    if (!res.ok) throw new Error (`Failed to fetch account ${res.status}`)
+    
+    return res.json()
+}
+
+export const getAccountProfile = async (puuid: string) => {
+    const res = await fetch(
+        `${mainUrl}getAccountProfile?puuid=${encodeURIComponent(
+            puuid
+        )}`
+    )
+    // const text = await res.text()
+    console.log('PUUID', puuid, res)
+    // console.error('Riot:', text)
+    if (!res.ok) throw new Error (`Failed to fetch account ${res.status}`)
+    
+    return res.json()
+}
+
+// RGAPI-7ce2128b-fac4-4454-9187-100434cee18b
